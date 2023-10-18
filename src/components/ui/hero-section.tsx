f81d37ba5cb9047c42b877ba8d7ceda77232e373
Amodio <pscamodio@gmail.com>
2023-10-18 19:46:47 +0000

    Update bio

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -5,7 +5,9 @@ export function HeroSection() : JSX.Element {
     return <section className={classes.section}>
         <aside>
             Hi, <br/>
-            I&apos;m Amodio Pesce, a software engineer and a technology enthusiast from Italy.
+            I&apos;m Amodio Pesce, a software engineer and a technology enthusiast from Italy. <br/>
+            I don&apos;t care if it is c++ or typescript, I thrive solving complex challenges in elegant ways <br/>
+            But personal connections and customers feedbacks are always more important than the development tools
         </aside>
         <div>
             <Image 
